95e10e76c311c2652b07bd8f1590c5e761f20799
zhanghuiblock <hui.zhang@blockin.com>
2018-05-14 21:26:56 +0800

    Revert "练习"
    This reverts commit 1265facad0b8a01f270f8a7384359f52dd56c5bd.

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import HelloWorld from '@/components/HelloWorld'
-import Template from '@/pages/test'
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'Template',
-      component: Template
+      name: 'HelloWorld',
+      component: HelloWorld
     }
   ]
 })
